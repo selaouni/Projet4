de96aff6f1b82603d9d2a8102a21e077cda65dd5
sabah <sabah.elaouni@gmail.com>
2022-09-05 17:18:48 +0200

    Code update (add, sort , split players, make matchs)

--- models/tournoi.py
+++ models/tournoi.py
@@ -13,7 +13,6 @@ class Tournoi:
         self.tour_list = tour_list
         self.timing = timing
         self.description = description
-
         self.db_tournoi = TinyDB('DB_tournoi.json')
 
 
@@ -40,20 +39,12 @@ class Tournoi:
         db_tournoi = self.db_tournoi
         db_tournoi.insert(tournoi.serialize())
 
-    def __repr__(self):
-        return f"{self.name} - {self.place} - {self.tour_list} - {self.timing} \n"
-
-    # def sorted_first_time(self):
-    #     print("-------------- sorted by rank ---------------")
-    #     self.player_list = sorted(self.player_list, key=lambda x: x.get('rank'))
-    #     print(self.player_list)
 
 
     def sorted_first_time(self,list_player_to_sort):
         player_list = pd.DataFrame(list_player_to_sort)
         player_list = player_list.sort_values(('Classement'),ascending=False)
         self.player_list = player_list.T.to_dict().values()
-        print("------- joueurs triés par classement")
         return(self.player_list)
 
 
@@ -61,43 +52,30 @@ class Tournoi:
     def split(self,sorted_player_to_split):
 
         half = len(sorted_player_to_split) // 2
-        player_splitted = sorted_player_to_split[:half], sorted_player_to_split[half:]
+        player_splitted = sorted_player_to_split[:half],sorted_player_to_split[half:]
         print("player split", player_splitted)
-        return player_splitted
+        return (player_splitted)
 
+    def make_match(self, player_splitted1 ,player_splitted2):
 
+        match_players = []
+        for i in range(len(self.player_list) // 2):
+            match_players.append(player_splitted1[i])
+            match_players.append(player_splitted2[i])
+        print("les matchs sont comme suit : ",match_players)
+        return player_splitted1, player_splitted2
 
     def sorted_second_time(self,):
-
-        self.players = sorted(self.players, key=lambda x: x.get('score'), reverse=True)
-
-
-    def make_match(self, top_players, bottom_players):
-        """Merge top and bottom players in order of matches
-        @param top_players: top half of players (list)
-        @param bottom_players: bottom half of players (list)
-        """
-        merged_players = []
-        for i in range(len(self.players) // 2):
-            merged_players.append(top_players[i])
-            merged_players.append(bottom_players[i])
-
-
+        pass
 
 
 
 
     def run_tournoi(self):
-        pass
+
         self.sorted_first_time()
         self.sorted_second_time()
-        #lancer le tournoi qui appelle le run de tour (créer l'object tournoi)
 
-        # While --> tant que (tous les joueurs n'ont pas jouer avec tous les autres joueurs')
-        #         si (1ere fois)
-        #         alors je lance la methode function sorted_firsttime()
-        #         sinon je lance la deuxieme_methode()
-        #       on va lancer un tour à chaque fois (la methode run () de tour)
 
 
 
